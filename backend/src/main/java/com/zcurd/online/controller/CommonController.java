e3ad7ad328ff9b3b23893e8af429331e4081965c
WYJ <2959380976@qq.com>
2023-12-15 11:34:17 +0800

    commit

--- backend/src/main/java/com/zcurd/online/controller/CommonController.java
+++ backend/src/main/java/com/zcurd/online/controller/CommonController.java
@@ -4,6 +4,7 @@ import com.jfinal.upload.UploadFile;
 import com.zcurd.account.common.annotation.ClearAuth;
 import com.zcurd.common.DBTool;
 import com.zcurd.common.base.BaseController;
+import com.zcurd.common.util.FileUploader;
 import com.zcurd.common.util.StringUtil;
 import com.zcurd.online.model.CommonFile;
 
@@ -13,37 +14,41 @@ import com.zcurd.online.model.CommonFile;
  */
 @ClearAuth
 public class CommonController extends BaseController {
-	
+
 	//获得字典数据
 	public void getDictData() {
 		Object[] queryParams = getQueryParams();
 		String[] properties = (String[]) queryParams[0];
 		String[] symbols = (String[]) queryParams[1];
 		Object[] values = (Object[]) queryParams[2];
-		
+
 		String orderBy = getOrderBy();
 		if(StringUtil.isEmpty(orderBy)) {
 			orderBy = "id desc";
 		}
-		
+
 		renderJson(DBTool.findByMultProperties("sys_dict", properties, symbols, values));
-	}	
-	
+	}
+
 	//图标页面
 	public void iconsPage() {
 		render("common/icons.html");
 	}
-	
+
 	//文件上传
 	public void uploadFile() {
 		UploadFile file = getFile("upload", "images", 1024 * 1024 * 5);	//上传文件不能大于5M
 		if(file != null) {
+			String url = FileUploader.upload(file);
 			//保存日志
 			new CommonFile().set("type", 1)	//1表示图片
 				.set("path", file.getFileName())
 				.set("sys_user_id", getSessionUser().get("id"))
 				.save();
-			renderJson("/upload/images/" + file.getFileName());
+
+			file.getFile().delete();
+//			renderJson("/upload/images/" + file.getFileName());
+			renderJson(url);
 		}else {
 			renderFailed();
 		}
